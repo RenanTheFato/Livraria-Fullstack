89ca1e0b1c41db6e35574d936486a8ac4d5b4b47
RenanTheFato <renanzintestes@gmail.com>
2024-08-29 11:10:28 -0300

    -

---
+++ client/src/components/HeaderGeneral.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-cyan-600">
+      <nav>oi</nav>
+    </header>
+  );
+}
+
+export default Header;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,17 @@
-import '../index.css'
+import "../index.css";
 
-function HomePage(){
-  return(
+function HomePage() {
+  return (
     <div className="w-full min-h-screen bg-stone-300">
-      
+      <header className="bg-cyan-600">
+        <nav>
+          <div>
+            <img className="w-16" src='./src/assets/logo.svg'/>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
 
-export default HomePage
+export default HomePage;
