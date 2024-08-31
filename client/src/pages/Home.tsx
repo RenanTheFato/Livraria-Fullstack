00eef62d54cbaef20397291622b190b4f8e6b76f
RenanTheFato <renanzintestes@gmail.com>
2024-08-30 21:22:57 -0300

    semi-finished login page, form is done, switch form is done, missing: aside banner and axios/backend connection

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { FaSearch } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { api } from "../service/api";
 import "../index.css";
 
@@ -41,20 +41,22 @@ function HomePage() {
       <header className="bg-cyan-600 shadow-lg md:max-2xl">
         <nav className="flex flex-row md:mx-auto">
           <div>
-            <img className="w-16 mx-3" src='./src/assets/logo.svg' alt="Logo"/>
+            <img className="w-16 mx-3" src="./src/assets/logo.svg" alt="Logo" />
           </div>
-          <h2 className="font-roboto-black text-sky-50 text-[1.5rem] my-3 mx-10 md:text-4xl">Livraria Encanto</h2>
+          <h2 className="font-roboto-black text-sky-50 text-[1.5rem] my-3 mx-10 md:text-4xl">
+            Livraria Encanto
+          </h2>
           <div className="relative w-1/3 h-10 my-3 mx-8 flex items-center shadow-xl">
-            <input 
-              type="text" 
+            <input
+              type="text"
               className="relative w-full py-2 pr-36 pl-5 rounded-xl border-2 outline-none border-black placeholder-gray-400 
-              placeholder:italic focus:bg-cyan-100 focus:placeholder-black focus:outline-none" 
-              placeholder="Pesquisar livro..." 
+              placeholder:italic focus:bg-cyan-100 focus:placeholder-black focus:outline-none"
+              placeholder="Pesquisar livro..."
             />
-            <button 
-              type="submit" 
+            <button
+              type="submit"
               className="absolute w-16 h-full right-0 mx-[0.12rem] bg-indigo-400 outline-none border-2 
-              border-r-transparent border-t-transparent border-b-transparent border-l-black flex items-center justify-center rounded-r-xl" 
+              border-r-transparent border-t-transparent border-b-transparent border-l-black flex items-center justify-center rounded-r-xl"
             >
               <FaSearch fill="#FFFFFF" />
             </button>
@@ -75,18 +77,32 @@ function HomePage() {
         <h1 className="font-roboto-italic italic text-2xl mx-14 w-[23rem] h-auto border-b-indigo-600 border-r-transparent border-t-transparent border-l-transparent border-2">
           RECOMENDADOS PARA VOCÊ
         </h1>
+
         <div className="flex justify-center items-center py-10">
           <section className="w-11/12 h-72 flex flex-row space-x-10 py-2 px-2 rounded-xl bg-gradient-to-t from-slate-200 to-slate-300">
             {displayedItems.map((item) => (
-              <article key={item.id} className="h-full w-72 bg-white rounded p-2 relative shadow-xl shadow-indigo-300
-              hover:scale-105 duration-200">
-                <p><span className="font-medium">Título: </span>{item.titulo}</p>
-                <p><span className="font-medium">Autor: </span>{item.autor}</p>
-                <p><span className="font-medium">Editora: </span>{item.editora}</p>
+              <article
+                key={item.id}
+                className="h-full w-72 bg-white rounded p-2 relative shadow-xl shadow-indigo-300
+              hover:scale-105 duration-200"
+              >
+                <p>
+                  <span className="font-medium">Título: </span>
+                  {item.titulo}
+                </p>
+                <p>
+                  <span className="font-medium">Autor: </span>
+                  {item.autor}
+                </p>
+                <p>
+                  <span className="font-medium">Editora: </span>
+                  {item.editora}
+                </p>
               </article>
             ))}
           </section>
         </div>
+        
       </div>
     </div>
   );
--- client/src/pages/LoginUser.tsx
+++ client/src/pages/LoginUser.tsx
@@ -1,23 +1,123 @@
-function LoginUser(){
-  return(
+import { useState } from "react";
+
+function LoginUser() {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  function LoginForm() {
+    return (
+      <>
+        <form className="flex flex-col w-full px-10 my-16 space-y-10">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-2 font-roboto rounded outline-none shadow-lg text-black focus:placeholder:text-black focus:shadow-cyan-300 focus:outline-1 focus:outline-teal-300"
+            placeholder="Insira seu email..."
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-2 font-roboto rounded outline-none shadow-lg text-black focus:placeholder:text-black focus:shadow-cyan-300 focus:outline-1 focus:outline-teal-300"
+            placeholder="Insira sua senha..."
+          />
+          <input
+            type="submit"
+            className="bg-blue-600 p-2 rounded font-bold font-roboto-bold text-sm cursor-pointer shadow-lg hover:bg-gradient-to-r hover:from-blue-600 hover:to-teal-400 hover:scale-105 hover:transition-all"
+            value="ENTRAR"
+          />
+        </form>
+        <div className="flex flex-row w-full relative">
+          <span className="text-black text-lg absolute mx-10">
+            Não tem um cadastro?
+          </span>
+          <span
+            className="text-blue-600 text-lg absolute right-0 mx-10 cursor-pointer"
+            onClick={handleFormSwitch}
+          >
+            Crie Já!
+          </span>
+        </div>
+      </>
+    );
+  }
+
+  function SingupForm() {
+    return (
+      <>
+        <form className="flex flex-col w-full px-10 my-14 space-y-8">
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="p-2 font-roboto rounded outline-none shadow-lg text-black focus:placeholder:text-black focus:shadow-cyan-300 focus:outline-1 focus:outline-teal-300"
+            placeholder="Insira seu nome..."
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-2 font-roboto rounded outline-none shadow-lg text-black focus:placeholder:text-black focus:shadow-cyan-300 focus:outline-1 focus:outline-teal-300"
+            placeholder="Insira seu email..."
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-2 font-roboto rounded outline-none shadow-lg text-black focus:placeholder:text-black focus:shadow-cyan-300 focus:outline-1 focus:outline-teal-300"
+            placeholder="Crie uma senha..."
+          />
+          <input
+            type="submit"
+            className="bg-blue-600 p-2 rounded font-bold font-roboto-bold text-sm cursor-pointer shadow-lg hover:bg-gradient-to-r hover:from-blue-600 hover:to-teal-400 hover:scale-105 hover:transition-all"
+            value="CADASTRAR"
+          />
+        </form>
+        <div className="flex flex-row w-full relative -my-5">
+          <span className="text-black text-lg absolute mx-10">
+            Já tem uma conta?
+          </span>
+          <span
+            className="text-blue-600 text-lg absolute right-0 mx-10 cursor-pointer"
+            onClick={handleFormSwitch}
+          >
+            Faça login
+          </span>
+        </div>
+      </>
+    );
+  }
+
+  function handleFormSwitch() {
+    setIsLogin(!isLogin);
+    setEmail("");
+    setPassword("");
+    setName("");
+  }
+
+  return (
     <main className="w-full min-h-screen bg-[url('./src/assets/rainbow-vortex.svg')] bg-no-repeat bg-cover text-white flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-2/5 bg-black">
-          oi
-        </aside>
-
+        <aside className="w-2/5 bg-black">oi</aside>
         <aside className="w-3/5 bg-slate-200 border-l-2 border-l-white bg-opacity-30 flex justify-center items-center">
           <div className="w-8/12 h-5/6 rounded-lg bg-slate-300 shadow-xl text-center">
-            <h1 className='font-roboto-bold text-5xl my-10'>LOGIN</h1>
-            <form>
+            <h1 className="font-roboto-bold text-4xl my-10">
+              {isLogin ? "LOGIN" : "CADASTRO"}
+            </h1>
+            <p className="font-roboto-medium text-black text-justify mx-10 -my-5 text-xl">
+              Bem vindo a livraria Encanto! Explore um novo mundo recheado de
+              histórias!
+            </p>
 
-            </form>
+            {isLogin ? LoginForm() : SingupForm()}
           </div>
         </aside>
-
       </div>
     </main>
   );
 }
 
-export default LoginUser
+export default LoginUser;
